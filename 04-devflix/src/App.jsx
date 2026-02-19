2e16026d0e3641e1c5978e3c7979480c28b7a6d9
ErickMerschbacher <erick.m.silva21@aluno.senai.br>
2026-02-19 11:20:01 -0300

    adicionando elementos á página inicial, iserindo logotipo, barra de pesquisa e um rodapé.
    Estilizando cada um dos elementos e melhorando alterações passadas dentro da pasta APP.jsx

--- 04-devflix/src/App.jsx
+++ 04-devflix/src/App.jsx
@@ -1,9 +1,23 @@
-import "./App.css"
+import Rodape from "./components/Rodape/Rodape";
+import "./App.css";
+import logo from "./assets/devflix.png";
+import lupa from "./assets/search.svg";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div id="App">
+      <img
+        className="logo"
+        src={logo}
+        alt="Logotipo da Netflix com fundo preto e letras vermelhas, representando a plataforma de streaming de filmes e séries"
+      />
+      <div className="search">
+        <input type="text" placeholder="Pesquise por filmes e séries..." />
+        <img src={lupa} alt="Botão de ação para Pesquisa!" />
+      </div>
+      <Rodape>ErickMerschbacher</Rodape>
+    </div>
+  );
+};
 
-export default App
+export default App;
