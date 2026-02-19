e6dcc6424cf6d392e10bd0bcdc7bc075a31d22c1
ErickMerschbacher <erick.m.silva21@aluno.senai.br>
2026-02-19 17:05:26 -0300

    Adiciona lógica para busca de filmes e arquivo de exemplo para chave da API OMDB

--- 04-devflix/src/App.jsx
+++ 04-devflix/src/App.jsx
@@ -2,8 +2,26 @@ import Rodape from "./components/Rodape/Rodape";
 import "./App.css";
 import logo from "./assets/devflix.png";
 import lupa from "./assets/search.svg";
+import { useEffect, useState } from "react";
 
 const App = () => {
+  const [movies, setMovies] = useState([]);
+  //utilizando uma chave de API do arquivo .env
+  const apiKey =  import.meta.enc.VITE_OMDB_API_KEY;  
+  const apiUrl = `https://omdpapi.com/?apikey=${apiKey}`;
+  //Criando a conexão coma a API e trazendo informações
+  const searchMovies = async (title) => {
+    const response = await fetch(`${apiUrl}&s=${title}`);
+    const data = await response.json;
+
+    //Alimentando a váriavel movies
+    setMovies(data.Search);
+  };
+
+  useEffect(()=>{
+    searchMovies("Batman"); 
+    
+  })
   return (
     <div id="App">
       <img
