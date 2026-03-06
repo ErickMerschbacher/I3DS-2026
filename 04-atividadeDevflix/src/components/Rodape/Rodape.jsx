942f9966e4fa5bc1c025b85ba905c1132f4f6203
ErickMerschbacher <erick.m.silva21@aluno.senai.br>
2026-03-06 16:42:04 -0300

    Atualiza o título da página para "Erick" e simplifica o componente Rodape removendo a imagem.

--- 04-atividadeDevflix/src/components/Rodape/Rodape.jsx
+++ 04-atividadeDevflix/src/components/Rodape/Rodape.jsx
@@ -4,14 +4,7 @@ import "./Rodape.module.css";
 const Rodape = ({ link, children }) => {
   return (
     <footer>
-      <p>
-        <img
-          src={Rodape}
-          alt="04-atividadeDevflix/src/assets/foto.png"
-          style={{ width: "200px" }}
-        />
-        Feito com 🏐 por <a href={link}>{children}</a>
-      </p>
+      Feito com 🏐 por <a href={link}>{children}</a>
     </footer>
   );
 };
