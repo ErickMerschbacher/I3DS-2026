68238c54a83d9e8a09200d4843acdd5fc087389b
ErickMerschbacher <erick.m.silva21@aluno.senai.br>
2026-03-12 16:28:10 -0300

    finalizamos a introdução

--- 05-react-router/src/App.jsx
+++ 05-react-router/src/App.jsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import { Route, Routes } from "react-router";
+import "./App.css";
+import Sobre from "./pages/Sobre";
+import Home from "./pages/Home";
+import Header from "./components/header/Header";
+import Contato from "./pages/Contato";
+import NaoEncontrado from "./pages/NaoEncontrado";
+import Footer from "./components/footer/Footer";
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <Header />
+      <div className="containerApp">
+        <Routes>
+          {/**identfica todas as rota do sistema */}
+          <Route path="/" element={<Home />} />
+          <Route path="/sobre" element={<Sobre />} /> {/*uma rota do sistema*/}
+          <Route path="/contato" element={<Contato />} />{" "}
+          {/*uma rota do sistema*/}
+          <Route path="/naoencontrado" element={<NaoEncontrado />} />{" "}
+          {/*uma rota do sistema*/}
+          <Route path="/footer" element={<Footer />} />{" "}
+          {/*uma rota do sistema*/}
+        </Routes>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      <Footer Link={"https://github.com/ErickMerschbacher"}>Erickin</Footer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
